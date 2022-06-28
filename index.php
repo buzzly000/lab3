6376dab9a59e8a80c84d546a5f9977c6153aaa8f
buzzly000 <108326554+buzzly000@users.noreply.github.com>
2022-06-28 14:23:35 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+Index.php
+    <html>
+    <Head>
+    <title> 1st PHP Page</title>
+    </head>
+    <body>
+    <?php
+    echo'<h1>Welcome to ATN!</h1>';
+?>
+    <button><args href='login.php'>login</button>
+    </form>
+    </body>
+    </html> 
